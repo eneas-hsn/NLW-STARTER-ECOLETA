ff4289bba6a645355b59bf059942ecc92cf9ea27
eneas-hsn <eneashsn@gmail.com>
2020-06-06 17:35:21 -0300

    Primeiros passos com o servidor de maneira correta

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const server=express()
 
+//configurar pasta pública
+server.use(express.static("public"))
+
 //ligar o servidor
 server.listen(3000)
 
@@ -9,4 +12,11 @@ server.listen(3000)
 //req é uma requisição e res é uma resposta
 server.get("/",(req,res)=> {
     res.sendFile(__dirname+"/views/index.html")
+})
+//a barra indica o caminho que o servidor vai seguir
+server.get("/create-point",(req,res)=> {
+    res.sendFile(__dirname+"/views/create-point.html")
+})
+server.get("/search-results",(req,res)=> {
+    res.sendFile(__dirname+"/views/search-results.html")
 })
