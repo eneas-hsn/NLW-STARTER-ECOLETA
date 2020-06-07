d6781a6ecbd459560a886f443ccfe96261ec367a
eneas-hsn <eneashsn@gmail.com>
2020-06-06 23:27:29 -0300

    Tempate engine aplicado

--- src/server.js
+++ src/server.js
@@ -7,16 +7,24 @@ server.use(express.static("public"))
 //ligar o servidor
 server.listen(3000)
 
+//utilizando template engine
+const nunjucks=require("nunjucks")
+nunjucks.configure("src/views",{
+    express:server,
+    noCache:true
+}) 
+
+//
 //configurar caminhos na minha aplicação
 //página inicial
 //req é uma requisição e res é uma resposta
 server.get("/",(req,res)=> {
-    res.sendFile(__dirname+"/views/index.html")
+    res.render("index.html")
 })
 //a barra indica o caminho que o servidor vai seguir
 server.get("/create-point",(req,res)=> {
-    res.sendFile(__dirname+"/views/create-point.html")
+    res.render("create-point.html")
 })
 server.get("/search-results",(req,res)=> {
-    res.sendFile(__dirname+"/views/search-results.html")
+    res.render("search-results.html")
 })
